a698930bcea1f0b625c0bb95598f4ead8f540a2f
MEGA-MAN-EXE\wolfe <mxv647@med.miami.edu>
2018-05-05 13:39:00 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,10 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL){
+  //Heroku Deployment
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  local host
 mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -12,6 +14,13 @@ mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
+// mysql.createConnection({
+//   port: 3306,
+//   host: "ocvwlym0zv3tcn68.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   user: "hjl47kl192nyafhp",
+//   password: "root",
+//   database: "burgers_db"
+// });
 };
 
 // Make connection.
